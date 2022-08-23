9e371777fefb354b11c2fc168bd64fdf08b5285d
Rama Shanker <rama.shanker@wirelesscar.com>
2022-08-23 22:57:13 +0200

    Writting Test cases

--- Day1/simple-maven-app/src/test/java/com/rama/data/AppTest.java
+++ Day1/simple-maven-app/src/test/java/com/rama/data/AppTest.java
@@ -1,38 +1,45 @@
 package com.rama.data;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
- * Unit test for simple App.
+ * Unit test for Greeting App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
+  Greeting greeting;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+  @Before
+  public void setUp() {
+    greeting = new Greeting();
+  }
+
+  @Test
+  public void TestHello() {
+
+    String result = greeting.sayHello();
+    assertEquals(result, "Hello");
+
+  }
+
+  @Test
+  public void TestHi() {
+    String result = greeting.sayHi();
+    assertEquals(result, "Hi");
+  }
+
+  @Test
+  public void TestWelcome() {
+    String result = greeting.sayWelcome();
+    assertEquals(result, "Welcome");
+  }
+
+  @Test
+  public void TestThanks() {
+    String result = greeting.sayThanks();
+    assertEquals(result, "Thanks");
+  }
 }
