12e6db5b176d1021bc82040c2275d684641cb4a4
sfjbs13june <sfjbs13june@gmail.com>
2022-08-30 09:32:10 +0000

    day test cases

--- Day6/student-microservice/src/test/java/com/student/app/StudentControllerTests.java
+++ Day6/student-microservice/src/test/java/com/student/app/StudentControllerTests.java
@@ -7,14 +7,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.util.Base64Utils;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
